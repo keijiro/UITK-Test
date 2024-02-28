eb6f86160d92ec0ab81ba7d935c0ea6b145d80e3
Keijiro Takahashi <keijiro@gmail.com>
2024-02-28 16:23:33 +0900

    Object list

--- Assets/RigidBodyListProvider.cs
+++ Assets/RigidBodyListProvider.cs
@@ -1,18 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
 using Unity.Properties;
 
-public sealed class TransformDataProvider : MonoBehaviour
+public sealed class RigidBodyListProvider : MonoBehaviour
 {
     [CreateProperty]
-    public string XformAsString
-      => $"{name}:\n - Position {transform.position}\n - Rotation {transform.eulerAngles}";
+    public List<string> ObjectList => EnumerateRigidBodies().ToList();
 
     [field:SerializeField]
     public UIDocument Document { get; set; }
 
     [field:SerializeField]
     public string Element { get; set; }
+
+    IEnumerable<string> EnumerateRigidBodies()
+      => FindObjectsByType<Rigidbody>(FindObjectsSortMode.InstanceID).Select(x => x.name);
 
     void Start()
       => Document.rootVisualElement.Q(Element).dataSource = this;
