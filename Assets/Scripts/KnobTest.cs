68fce2b94bec15767b227b707824638b8d3b3594
Keijiro Takahashi <keijiro@gmail.com>
2024-04-04 16:08:19 +0900

    Build to iPhone

--- Assets/Scripts/KnobTest.cs
+++ Assets/Scripts/KnobTest.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
-using UnityEditor;
 using UnityEngine.UIElements;
 using Unity.Properties;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public sealed class KnobTest : MonoBehaviour
 {
@@ -20,15 +23,19 @@ public sealed class KnobTest : MonoBehaviour
     void Start()
       => GetComponent<UIDocument>().rootVisualElement.dataSource = this;
 
+    void LateUpdate()
+      => Target.localRotation =
+           Quaternion.Euler(Parameter1, Parameter2, Parameter3);
+
+#if UNITY_EDITOR
     [InitializeOnLoadMethod]
+#else
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+#endif
     public static void RegisterConverters()
     {
         var grp = new ConverterGroup("Knob 0-100");
         grp.AddConverter((ref float v) => $"{v:0.00}");
         ConverterGroups.RegisterConverterGroup(grp);
     }
-
-    void LateUpdate()
-      => Target.localRotation =
-           Quaternion.Euler(Parameter1, Parameter2, Parameter3);
 }
